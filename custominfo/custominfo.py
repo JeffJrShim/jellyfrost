ff5a4f1994b47e18a09f7bc24968220e799e9381
JeffJrShim <71493766+JeffJrShim@users.noreply.github.com>
2022-03-05 22:40:30 +0800

    Well, make sure the code *actually* works first

--- custominfo/custominfo.py
+++ custominfo/custominfo.py
@@ -38,7 +38,7 @@ class CustomInfo(commands.Cog):
         
     @commands.command()
     async def info(self, ctx):
-        owner = self.bot.get_user(726802094371242074)
+        owner = bot.get_user(726802094371242074)
         embed_links = await ctx.embed_requested()
         author_repo = "https://github.com/Twentysix26"
         org_repo = "https://github.com/Cog-Creators"
@@ -52,16 +52,6 @@ class CustomInfo(commands.Cog):
         python_version = "[`{}.{}.{}`]({})".format(*sys.version_info[:3], python_url)
         dpy_version = "[`{}`]({})".format(discord.__version__, dpy_repo)
         red_version = "[`{}`]({})".format(version_info, red_pypi)
-        embed = discord.Embed(color=await ctx.embed_color())
-        embed.add_field(name=f"{ctx.me.name}'s Owner", value=owner)
-        embed.add_field(
-            name="Versions",
-            value=f"<:Python:928926404077289533> {python_version}\n<:dpy:926811146101596222> {dpy_version}\n<:red:926316873400872990> {red_version}",
-        )
-        embed.add_field(
-            name=f"About {ctx.me.name}",
-            value=f"{ctx.me.name} is an instance of [Red, an open source Discord bot]({red_repo}) created by [Twentysix]({author_repo}) and improved by many with many 3rd party cogs, written by many other Cog Creators, and {owner.name} himself. Please do not bother Red's support server with inquiries regarding this bot, please use the [support server]({support_server_url}), instead. \n\nRed is backed by a passionate community who contributes and creates content for everyone to enjoy. Join us today and help us improve!\n\n(c) Cog Creators, and {owner.name}",
-            inline=False,
-        )
+        embed = discord.Embed(description=f"**:wave: Hi, I am {ctx.me.name}!**\nI am currently running on version **__{red_version}__**\n**Support Server**\nJoin me on my support server: [Toli Support]({support_server_url})\n**Invite me to your Server**\nHere is a link to invite me to your guild as well: [Toli invite link](https://discord.com/api/oauth2/authorize?client_id=943931974568001546&permissions=0&scope=bot)\n**I am listed in follow bot lists:**\n:white_check_mark: [DBL (top.gg)](https://top.gg/bot/943931974568001546)\n✅ [Discord Bots](https://discord.bots.gg/bots/943931974568001546)\n✅ [discordbotlist.com](https://discordbotlist.com/bots/toli)\n✅ [Bots on Discord](https://bots.ondiscord.xyz/bots/943931974568001546)\n✅ [Bots for Discord](https://botsfordiscord.com/bot/943931974568001546)\nI am on **{len(bot.guilds)}** guilds!" ,color=await ctx.embed_color())
         embed.set_thumbnail(url=ctx.me.avatar_url)
         await ctx.send(embed=embed, reference=ctx.message.to_reference())
